cdd61cb8c6cdbe67df8a06ada226c148d1c0e9f8
NycuRO <nghergu@yahoo.com>
2017-03-27 11:17:36 +0300

    Create BiomeTypes.php

---
+++ src/pocketmine/level/generator/biome/BiomeTypes.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Added by NycuRO on 27.03.2017 */
+
+namespace pocketmine\level\generator\biome;
+
+abstract class BiomeTypes
+{
+
+	const OCEAN;
+  const COLD;
+  const MEDIUM;
+  const WARM ;
+
+}
