611813013f0933e09fd602845b8855cd934ae214
NycuRO <nghergu@yahoo.com>
2017-03-27 12:03:16 +0300

    Update OceanBiome.php

--- src/pocketmine/level/generator/normal/biome/extension/OceanBiome.php
+++ src/pocketmine/level/generator/normal/biome/extension/OceanBiome.php
@@ -27,7 +27,7 @@ use pocketmine\level\generator\normal\populator\Mushroom;
 use pocketmine\level\generator\normal\populator\SugarCane;
 use pocketmine\level\generator\normal\populator\TallGrass;
 
-class OceanBiome extends WateryBiome
+class OceanBiome extends pocketmine\level\generator\normal\biome\WateryBiome
 {
 
 	public function __construct()
