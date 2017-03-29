bb858e5fe47d813c1061a4d65425a3aecf4d2285
NycuRO <nghergu@yahoo.com>
2017-03-29 11:33:50 +0300

    Update GrassyBiome.php

--- src/pocketmine/level/generator/normal/biome/GrassyBiome.php
+++ src/pocketmine/level/generator/normal/biome/GrassyBiome.php
@@ -19,13 +19,17 @@
  *
 */
 
+/* Modificated by NycuRO on 29.03.2017 */
+
 namespace pocketmine\level\generator\normal\biome;
 
 use pocketmine\block\Block;
 
-abstract class GrassyBiome extends NormalBiome{
+abstract class GrassyBiome extends NormalBiome
+{
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->setGroundCover([
 			Block::get(Block::GRASS, 0),
 			Block::get(Block::DIRT, 0),
