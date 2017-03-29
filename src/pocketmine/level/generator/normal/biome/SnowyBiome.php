e5ba0166a927ce895e9cd663aa9a023f3206eb95
NycuRO <nghergu@yahoo.com>
2017-03-29 11:36:00 +0300

    Update SnowyBiome.php

--- src/pocketmine/level/generator/normal/biome/SnowyBiome.php
+++ src/pocketmine/level/generator/normal/biome/SnowyBiome.php
@@ -19,13 +19,17 @@
  *
 */
 
+/* Modificated by NycuRO on 29.03.2017 */
+
 namespace pocketmine\level\generator\normal\biome;
 
 use pocketmine\block\Block;
 
-abstract class SnowyBiome extends NormalBiome{
+abstract class SnowyBiome extends NormalBiome
+{
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->setGroundCover([
 			Block::get(Block::SNOW_LAYER, 0),
 			Block::get(Block::GRASS, 0),
