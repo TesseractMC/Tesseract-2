1733365f83ea9f962798a1d1bd4f1d2032b7ec6c
NycuRO <nghergu@yahoo.com>
2017-04-11 20:10:22 +0300

    Namespace and name, srry

--- src/pocketmine/level/generator/normal/populator/Clay.php
+++ src/pocketmine/level/generator/normal/populator/Clay.php
@@ -12,36 +12,36 @@ use pocketmine\level\ChunkManager;
 use pocketmine\level\generator\populator\VariableAmountPopulator;
 use pocketmine\utils\Random;
 
-class Mushroom extends VariableAmountPopulator
+class Clay extends VariableAmountPopulator
 {
 
 	private $level;
 
 	public function __construct()
-  {
+	{
 		parent::__construct(1, 0, 64);
 	}
 
 	public function populate(ChunkManager $level, $chunkX, $chunkZ, Random $random)
-  {
+	{
 		if(!$this->checkOdd($random))
-    {
+    	{
 			return;
 		}
 		$this->level = $level;
 		$amount = $this->getAmount($random);
 
 		for($i = 0; $i < $amount; ++$i)
-    {
+    	{
 			$x = $chunkX << 4;
 			$z = $chunkZ << 4;
 			for($size = 20; $size > 0; $size--)
-      {
+      		{
 				$xx = $x - 7 + $random->nextRange(0, 15);
 				$zz = $x - 7 + $random->nextRange(0, 15);
 				$yy = $this->getHighestWorkableBlock($xx, $zz);
 				if($yy !== -1)
-        {
+        		{
 					$this->level->setBlockIdAt($xx, $yy, $zz, Clay::CLAY_BLOCK);
 				}
 			}
@@ -49,12 +49,12 @@ class Mushroom extends VariableAmountPopulator
 	}
 
 	private function getHighestWorkableBlock($x, $z)
-  {
+	{
 		for($y = 256; $y >= 0; --$y)
-    {
+    	{
 			$b = $this->level->getBlockIdAt($x, $y, $z);
 			if($b === Block::WATER)
-      {
+      		{
 				break;
 			}
 		}
